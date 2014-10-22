f3bf01b15a5a1a6bed9dc93c2b32567fecf6caaf
jcheun <jcheun@ucsc.edu>
2014-10-21 17:01:08 -0700

    Node Server

---
+++ split-server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+var users = [{ FirstName: "Jesse",
+               LastName: "Cheun"
+             },
+             {
+               FirstName: "Ephraim",
+               LastName: "Chu"
+             }];
+
+var router = express.Router();
+var usersRoute = router.route('/users');
+
+usersRoute.get(function(request, response) {
+  response.send(users);
+});
+
+usersRoute.put(function(request, response) {
+   users.push({ FirstName: request.body.FirstName,
+                LastName: request.body.LastName 
+              });
+   response.send(users[users.length - 1]);
+});
+
+usersRoute.delete(function(request, response) {
+   response.send(users.splice(parseInt(request.body.user_id),1))
+});
+
+app.use('/test', router);
+app.listen(8888);
