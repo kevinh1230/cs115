51d5ec6e905cd5ca6322302865a8cdf2e3f5365f
khuang6@ucsc.edu <khuang6@ucsc.edu>
2014-11-08 20:18:21 -0800

    started to integrate FE/BE

--- SplitServer/public/js/controllers/ProfileCtrl.js
+++ SplitServer/public/js/controllers/ProfileCtrl.js
@@ -1,4 +1,15 @@
-angular.module('ProfileCtrl', []).controller('ProfileController', function($scope, $http, $location) {
+var app = angular.module('ProfileCtrl', [])
+app.controller('ProfileController', function ($scope, $http, $location) {
+
+	this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
 
 	$http.get('/user').success(function(user) {
 			$scope.user = user;
@@ -59,3 +70,116 @@ angular.module('ProfileCtrl', []).controller('ProfileController', function($scop
              });
     }
 });
+
+app.controller('createBill', function ($scope, $modal, $log) {
+    $scope.bill = {title: "",
+      type: "",
+      splitters:"",
+      amount:"",
+      comments:"",
+     }
+
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'createBill.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+       resolve: {
+         bill: function () {
+          return $scope.bill;
+         }
+       }
+    });
+
+   modalInstance.result.then(function () {
+       //$scope.bill = bill;
+    }, function () {
+      $log.info('ModalInstanceCtrl dismissed at: ' + new Date() + "create");
+    });
+  };
+  });
+
+  app.controller('ModalInstanceCtrl', function ($scope, $modalInstance,bill) {
+    $scope.bill = bill;
+    $scope.ok = function () {
+      $modalInstance.close();
+    };
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  });
+
+  app.controller('payBill', function ($scope, $modal, $log){
+    $scope.pay = {status: "", method: ""}
+
+    $scope.open = function () {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'payBill.html',
+        controller: 'payBillModalInstanceCtrl',
+        resolve: {
+          pay: function () {
+            return $scope.pay;
+          }
+        }
+      });
+
+      modalInstance.result.then(function () {
+        //$scope.user = {}; //reset form
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  });
+
+  app.controller('payBillModalInstanceCtrl', function ($scope, $modalInstance, pay) {
+    $scope.pay = pay;
+    $scope.ok = function () {
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  });
+
+  app.controller('addFriend', function ($scope, $modal, $log) {
+
+    $scope.user = {name: ""}
+
+    $scope.open = function () {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'addFriend.html',
+        controller: 'AddFriendModalInstanceCtrl',
+        resolve: {
+          user: function () {
+            return $scope.user;
+          }
+        }
+      });
+
+      modalInstance.result.then(function () {
+        //$scope.user = {}; //reset form
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+  app.controller('AddFriendModalInstanceCtrl', function ($scope, $modalInstance, user) {
+    $scope.user = user;
+    $scope.ok = function () {
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  });
+ 
